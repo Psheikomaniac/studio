fe4d6bdc720d87667358be33e4e2c634e154b78d
Heiko Rademacher <rademacher.heiko@gmail.com>
2025-10-20 22:00:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9498911553-e6834",
+  "appId": "1:358916918755:web:3107a844f24c72d9e464d6",
+  "apiKey": "AIzaSyDjhSxCRUhOsNZMiKxuZEAHwcLLIJU3Av4",
+  "authDomain": "studio-9498911553-e6834.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "358916918755"
+};
